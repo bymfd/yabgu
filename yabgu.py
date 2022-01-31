90b5d6012cabb027247d29b48dc83db17220e55d
mevlüt furkan demir <frukolosko@gmail.com>
2022-01-31 22:45:24 +0300

    add log and api handle error

--- yabgu.py
+++ yabgu.py
@@ -9,20 +9,23 @@ import shutil
 import glob
 import tldextract
 import argparse
+import sys
 from sys import platform as _platform
 from datetime import date
+import logging
 
 # Suppress https warning (Burp)
 requests.packages.urllib3.disable_warnings(category=InsecureRequestWarning)
 config = configparser.ConfigParser()
 config.read('config.ini')
+
 parser = argparse.ArgumentParser(
-    description='Yabgu - Zerossl - apache ve plesk için SSL sertifika yöneticisi'
+    description='Yabgu - Zerossl - apache ve plesk icin SSL sertifika yoneticisi'
 )
-parser.add_argument('-s', '--site', metavar='site', required=False, help='Kayıt edilecek domain')
-parser.add_argument('-l', '--list', metavar='list', required=False, help='Kayıtlı domain listesi')
-parser.add_argument('-r', '--renew', metavar='renew', required=False, help='Kayıtlı domainleri yenile')
-parser.add_argument('-rm', '--remove', metavar='remove', required=False, help='Kayıtlı domaini revoke et ve sil')
+parser.add_argument('-s', '--site', metavar='site', required=False, help='Kayit edilecek domain')
+parser.add_argument('-l', '--list', metavar='list', required=False, help='Kayitli domain listesi')
+parser.add_argument('-r', '--renew', metavar='renew', required=False, help='Kayitli domainleri yenile')
+parser.add_argument('-rm', '--remove', metavar='remove', required=False, help='Kayitli domaini revoke et ve sil')
 
 args = parser.parse_args()
 path_sep = "\\"
@@ -39,29 +42,37 @@ today = date.today()
 d1 = today.strftime("%m.%Y")
 
 domain = ''
+domains = ""
+logging.basicConfig(filename=f'log{path_sep}yabgu-' + today.strftime("%d-%m-%Y") + '.log',
+                    format='%(asctime)s %(message)s',
+                    level=logging.DEBUG)
+
+logging.debug(args)
 
 
 class SSLCertReNew(object):
 
     def __init__(self):
-        global domain
+        global domain, domains
         self.url = config['oturum']['api_end_point']
         self.proxies = None
         self.apiKey = config['oturum']['api_key']
         if args.site is not None:
+            logging.debug(args.site)
+            domains = args.site.split(",")
             domain = args.site
+            logging.debug(domain)
+            logging.debug(domains)
         else:
+            domains = domain.split(",")
             domain = domain
-        self.certificateDomain = domain
-        self.commonName = domain
-        self.requestSites = domain
-        ext = tldextract.extract(domain)
-        if (ext.subdomain == ""):
-            self.certificateDomain = "DNS: " + self.certificateDomain + ",DNS: www." + self.certificateDomain
-            self.requestSites = self.requestSites + ",www." + self.requestSites
-        else:
-            self.certificateDomain = "DNS: " + self.certificateDomain
-        print(self.certificateDomain)
+            logging.debug(domain)
+            logging.debug(domains)
+        self.certificateDomain = ""
+        self.commonName = ""
+        self.requestSites = ""
+        self.organization=""
+        self.makeDomain()
         self.csr = self.createCsr()
         # run steps
         self.InitialRequest()
@@ -71,7 +82,18 @@ class SSLCertReNew(object):
         else:
             self.DownloadAndSave()
 
+    def makeDomain(self):
+        print(domains)
+        self.commonName = domains[0].strip()
+        self.certificateDomain = "DNS: " + domain.replace(",", ",DNS: ").strip()
+        self.requestSites = domain.strip()
+        self.organization=tldextract.extract(self.commonName)
+        # print(self.commonName)
+        # print(self.certificateDomain)
+        # print(self.requestSites)
+
     def createCsr(self):
+        logging.debug("crt create")
         req = f'''
 [ req ]
 default_bits = 2048
@@ -98,15 +120,18 @@ subjectAltName = {self.certificateDomain}'''
         f = open(f'{self.commonName}.conf', 'w+')
         f.write(req)
         f.close()
+        logging.debug("openssl crt req")
         subprocess.Popen(['openssl', 'req', '-new', '-config', f'{self.commonName}.conf', '-keyout',
                           f'{self.commonName}_key.pem', '-out', f'{self.commonName}.csr'])
         time.sleep(3)
         # read csr
         with open(f'{self.commonName}.csr', 'r') as file:
             data = file.read().replace('\n', '')
+            logging.debug(data)
         return data
 
     def InitialRequest(self):
+        logging.debug("init request")
         with open(f'{self.commonName}.csr', 'r') as file:
             csrf = file.read().replace('\n', '')
         response = requests.post(self.url + f'/certificates?access_key={self.apiKey}',
@@ -115,15 +140,19 @@ subjectAltName = {self.certificateDomain}'''
                                        'certificate_validity_days': 90,
                                        'certificate_csr': csrf}
                                  )
+        logging.debug(response.text)
         result = json.loads(response.text)
-        print(result["success"])
-        if result["success"]:
+        success = "success" in result
+
+        if success is not True:
             self.certHash = result['id']
+            logging.debug("cert id" + result["id"])
         else:
             print("error " + str(result["error"]["code"]) + " : " + result["error"]["type"])
-            exit(2)
-        self.certHash = result['id']
-
+            logging.debug("error " + str(result["error"]["code"]) + " : " + result["error"]["type"])
+            logging.debug("exit")
+            sys.exit()
+       # self.certHash = result['id']
         # url from json
         self.HttpsUrl = result['validation']['other_methods'][f'{self.commonName}']['file_validation_url_https']
         self.HttpsContent = result['validation']['other_methods'][f'{self.commonName}']['file_validation_content']
@@ -189,7 +218,7 @@ subjectAltName = {self.certificateDomain}'''
 
 class SSLCertReNewControl(object):
     def __init__(self):
-        print("Bu parametre geliştirme aşamasında\n"
+        print("Bu parametre gelistirme asamasinda\n"
               "*********\n"
               "Yabgu\n"
               "*********\n"
@@ -198,7 +227,7 @@ class SSLCertReNewControl(object):
 
 class SSLCertReNewList(object):
     def __init__(self):
-        print("Bu parametre geliştirme aşamasında\n"
+        print("Bu parametre gelistirme asamasinda\n"
               "*********\n"
               "Yabgu\n"
               "*********\n"
@@ -207,7 +236,7 @@ class SSLCertReNewList(object):
 
 class SSLCertRemove(object):
     def __init__(self):
-        print("Bu parametre geliştirme aşamasında\n"
+        print("Bu parametre gelistirme asamasinda\n"
               "*********\n"
               "Yabgu\n"
               "*********\n"
